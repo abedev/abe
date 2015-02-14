421eb3c7d6c5c0cd43280353a5139d18e3c97525
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-14 09:22:20 -0700

    notes

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Build REST apis with Haxe and nodejs.
 
 WARNING: this is an experimental project and might be dropped at any time.
+
+
+TODO:
+  * parse and validate parameters
+    * type requirements
+    * custom validators
+    * filters? String -> Int, String -> Enum, ...
--- test/routes/Index.hx
+++ test/routes/Index.hx
@@ -10,4 +10,24 @@ class Index implements IRoute {
   public function manual() {
     response.send("Hello World");
   }
+
+  /*
+    // passport
+    // server.render ?
+    // capture app error
+    // capture HttpErrors
+
+    @:get/@:post/@:put/@:options/@:head/@:delete
+    @:method(a,b,c)
+
+    @:path("/")
+
+    @:param("name", String) // not needed, can be inferred
+
+    @:name("string") // for forwarding to next("string")
+
+    @:version("1.1.3")
+
+    @:role()
+  */
 }
