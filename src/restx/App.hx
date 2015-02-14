30a247d5b2cede7656c0af9bfaaed1069f32a07f
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-14 00:57:21 -0700

    server init

--- src/restx/App.hx
+++ src/restx/App.hx
@@ -1,15 +1,25 @@
 package restx;
 
+import restify.Restify;
+
 class App {
   public var port(default, null) : Int;
   public var router(default, null) : Router;
+
+  var server : Dynamic;
   public function new(port : Int) {
     this.port = port;
-    this.router = new Router();
     this.server = Restify.createServer({
       name : "RESTX"
     });
+    this.router = new Router(server);
   }
 
-  public function start() { }
+  public function start(?callback : Void -> Void) {
+    server.listen(port, function() {
+      trace('${server.name} listening on ${server.url}');
+      if(null != callback)
+        callback();
+    });
+  }
 }
