7cdcf887b15ef6cf66fd46464569142c2a441f37
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-14 00:57:09 -0700

    basic routing feature

--- src/restx/Router.hx
+++ src/restx/Router.hx
@@ -1,11 +1,34 @@
 package restx;
 
 import haxe.Constraints.Function;
+using thx.core.Nulls;
 
 class Router {
-  public function new() {}
-
-  public function register(path : String, controller : Function) {
-
+  var server : Dynamic;
+  public function new(server : Dynamic) {
+    this.server = server;
   }
+
+  public function register(info : RouteInfo) {
+    var method = info.method.or("GET").toLowerCase();
+    Reflect.callMethod(
+      server,
+      Reflect.field(server, method), [
+        info.path,
+        function(req, res, next) {
+          info.instance.request = req;
+          info.instance.response = res;
+          info.controller();
+          next();
+        }
+      ]
+    );
+  }
+}
+
+typedef RouteInfo = {
+  path : String,
+  method : String,
+  instance : IRoute,
+  controller : Function
 }
