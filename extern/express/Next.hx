9ad3c725168c0d1bb7f0ba94ccab49c6db8e0e69
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-14 14:59:08 -0700

    more types for Next

--- extern/express/Next.hx
+++ extern/express/Next.hx
@@ -1,9 +1,13 @@
 package express;
 
+import js.Error;
+
 typedef FNext = Void -> Void;
 typedef FNextRoute = String -> Void;
+typedef FNextError = Error -> Void;
 
 abstract Next(Dynamic)
   from FNext to FNext
   from FNextRoute to FNextRoute
+  from FNextError to FNextError
 { }
