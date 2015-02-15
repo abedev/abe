afab97395b9a98849fe34e1c09d117bc7c1e4749
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-14 17:03:03 -0700

    more cleanup

--- test/routes/Auto.hx
+++ test/routes/Auto.hx
@@ -8,8 +8,5 @@ import express.Response;
 import restx.IRoute;
 
 class Auto implements IRoute {
-  public var request : Request;
-  public var response : Response;
-  public var next : Next;
-  public function new() {}
+
 }
