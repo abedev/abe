8d2867809ef64caf74e3f6a0339a2395cd459692
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-21 18:33:24 -0700

    moved Express to independent library

--- src/express/Middleware.hx
+++
@@ -1,9 +0,0 @@
-package express;
-
-typedef FMiddlware = Response -> Request -> Void;
-typedef FMiddlwareNext = Response -> Request -> Next -> Void;
-
-abstract Middleware(Dynamic)
-  from FMiddlware to FMiddlware
-  from FMiddlwareNext to FMiddlwareNext
-{ }
