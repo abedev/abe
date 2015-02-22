3c1a74f985817be6667cc38a80732fa1434321b1
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-22 13:33:31 -0700

    added remaining express methods

--- src/abe/Method.hx
+++ src/abe/Method.hx
@@ -2,12 +2,30 @@ package abe;
 
 @:enum abstract Method(String) from String to String {
   var All = "all";
-  var Get = "get";
-  var Post = "post";
-  var Head = "head";
-  var Options = "options";
-  var Put = "put";
-  var Delete = "delete";
-  var Trace = "trace";
+  var Checkout = "checkout";
   var Connect = "connect";
+  var Copy = "copy";
+  var Delete = "delete";
+  var Get = "get";
+  var Head = "head";
+  var Lock = "lock";
+  var Merge = "merge";
+  var MkActivity = "mkactivity";
+  var MkCol = "mkcol";
+  var Move = "move";
+  var MSearch = "m-search";
+  var Notify = "notify";
+  var Options = "options";
+  var Patch = "patch";
+  var Post = "post";
+  var PropFind = "propfind";
+  var PropPatch = "proppatch";
+  var Purge = "purge";
+  var Put = "put";
+  var Report = "report";
+  var Search = "search";
+  var Subscribe = "subscribe";
+  var Trace = "trace";
+  var Unlock = "unlock";
+  var Unsubscribe = "unsubscribe";
 }
--- src/abe/Methods.hx
+++ src/abe/Methods.hx
@@ -1,5 +1,33 @@
 package abe;
 
 class Methods {
-  public static var list(default, never) : Iterable<Method> = [Get, Post, Head, Options, Put, Delete, Trace, Connect, All];
+  public static var list(default, never) : Iterable<Method> = [
+    All,
+    Checkout,
+    Connect,
+    Copy,
+    Delete,
+    Get,
+    Head,
+    Lock,
+    Merge,
+    MkActivity,
+    MkCol,
+    Move,
+    MSearch,
+    Notify,
+    Options,
+    Patch,
+    Post,
+    PropFind,
+    PropPatch,
+    Purge,
+    Put,
+    Report,
+    Search,
+    Subscribe,
+    Trace,
+    Unlock,
+    Unsubscribe
+  ];
 }
