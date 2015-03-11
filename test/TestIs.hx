ef779373785a70727b1d059a31c3a66370da1923
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-11 17:22:41 -0600

    added test for @:is()

---
+++ test/TestIs.hx
@@ -0,0 +1,40 @@
+import express.Error;
+import express.Next;
+import express.Request;
+import express.Response;
+import routes.*;
+import utest.Assert;
+
+class TestIs extends TestCalls {
+  public function testIs() {
+    router.register(new Is());
+
+    get("/",
+      { "Content-Type" : "text/html" },
+      function (msg, r) {
+        Assert.stringContains("text/html", Reflect.field(r.headers, "content-type"));
+        Assert.equals("<html></html>", msg);
+      });
+
+    get("/",
+      { "Content-Type" : "application/json" },
+      function (msg, r) {
+        Assert.stringContains("application/json", Reflect.field(r.headers, "content-type"));
+        Assert.equals("{\"name\":\"is\"}", msg);
+      });
+  }
+}
+
+class Is implements abe.IRoute {
+  @:get("/")
+  @:is("html")
+  function getHtml() {
+    response.send('<html></html>');
+  }
+
+  @:get("/")
+  @:is("json")
+  function getJson() {
+    response.send({ name : "is" });
+  }
+}
